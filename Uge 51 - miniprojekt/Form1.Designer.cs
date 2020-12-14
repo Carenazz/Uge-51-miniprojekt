410293d6cd60e7efae5f40e8b54b875f414032a9
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-12-14 11:22:07 +0100

    Update Form1.Designer.cs

--- Uge 51 - miniprojekt/Form1.Designer.cs
+++ Uge 51 - miniprojekt/Form1.Designer.cs
@@ -35,11 +35,20 @@ namespace Uge_51___miniprojekt
             this.pepperoniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.hawaiiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.meatLoverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.label1 = new System.Windows.Forms.Label();
             this.bestilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.hurtigstMuligtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bestemtTidToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menukortPizzaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,6 +66,8 @@ namespace Uge_51___miniprojekt
             // 
             // pizzaToolStripMenuItem
             // 
+            this.pizzaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menukortPizzaToolStripMenuItem});
             this.pizzaToolStripMenuItem.Name = "pizzaToolStripMenuItem";
             this.pizzaToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
             this.pizzaToolStripMenuItem.Text = "Menu";
@@ -90,6 +101,27 @@ namespace Uge_51___miniprojekt
             this.meatLoverToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.meatLoverToolStripMenuItem.Text = "Hawaii";
             // 
+            // bestilToolStripMenuItem
+            // 
+            this.bestilToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hurtigstMuligtToolStripMenuItem,
+            this.bestemtTidToolStripMenuItem});
+            this.bestilToolStripMenuItem.Name = "bestilToolStripMenuItem";
+            this.bestilToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.bestilToolStripMenuItem.Text = "Bestil";
+            // 
+            // hurtigstMuligtToolStripMenuItem
+            // 
+            this.hurtigstMuligtToolStripMenuItem.Name = "hurtigstMuligtToolStripMenuItem";
+            this.hurtigstMuligtToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.hurtigstMuligtToolStripMenuItem.Text = "Hurtigst muligt";
+            // 
+            // bestemtTidToolStripMenuItem
+            // 
+            this.bestemtTidToolStripMenuItem.Name = "bestemtTidToolStripMenuItem";
+            this.bestemtTidToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.bestemtTidToolStripMenuItem.Text = "Bestemt tid";
+            // 
             // listView1
             // 
             this.listView1.HideSelection = false;
@@ -108,26 +140,83 @@ namespace Uge_51___miniprojekt
             this.label1.TabIndex = 2;
             this.label1.Text = "Pepperoni";
             // 
-            // bestilToolStripMenuItem
+            // label2
             // 
-            this.bestilToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.hurtigstMuligtToolStripMenuItem,
-            this.bestemtTidToolStripMenuItem});
-            this.bestilToolStripMenuItem.Name = "bestilToolStripMenuItem";
-            this.bestilToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
-            this.bestilToolStripMenuItem.Text = "Bestil";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Alm. 60.-";
             // 
-            // hurtigstMuligtToolStripMenuItem
+            // label3
             // 
-            this.hurtigstMuligtToolStripMenuItem.Name = "hurtigstMuligtToolStripMenuItem";
-            this.hurtigstMuligtToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.hurtigstMuligtToolStripMenuItem.Text = "Hurtigst muligt";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(251, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Familie 110.-";
             // 
-            // bestemtTidToolStripMenuItem
+            // label4
             // 
-            this.bestemtTidToolStripMenuItem.Name = "bestemtTidToolStripMenuItem";
-            this.bestemtTidToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.bestemtTidToolStripMenuItem.Text = "Bestemt tid";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(251, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Skinke";
+            // 
+            // menukortPizzaToolStripMenuItem
+            // 
+            this.menukortPizzaToolStripMenuItem.Name = "menukortPizzaToolStripMenuItem";
+            this.menukortPizzaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.menukortPizzaToolStripMenuItem.Text = "Menukort - Pizza";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(251, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Alm. 55.-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(251, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Familie 105.-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(251, 222);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Hawaii";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(251, 245);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Alm. 70.-";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(251, 262);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Familie 120.-";
             // 
             // DashboardForm
             // 
@@ -135,6 +224,14 @@ namespace Uge_51___miniprojekt
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(761, 721);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.menuStrip1);
@@ -162,6 +259,15 @@ namespace Uge_51___miniprojekt
         private System.Windows.Forms.ToolStripMenuItem bestilToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem hurtigstMuligtToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem bestemtTidToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripMenuItem menukortPizzaToolStripMenuItem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
