89cf1fe4c4d968af8292eb44135d3f305d67aeb6
Carenazz <73272083+Carenazz@users.noreply.github.com>
2021-01-05 07:45:36 +0100

    Testing area.

--- Uge 51 - miniprojekt/Form1.cs
+++ Uge 51 - miniprojekt/Form1.cs
@@ -263,5 +263,18 @@ namespace Uge_51___miniprojekt
         {
             MessageBox.Show("Test");
         }
+
+        // Testing things.
+
+        private void TestButton_Click(object sender, EventArgs e)
+        {
+            List<Button> buttons = new List<Button>();
+            for (int i = 0; i < 20; i++)
+            {
+                Button newButton = new Button();
+                buttons.Add(newButton);
+                this.Controls.Add(newButton);
+            }
+        }
     }
 }
