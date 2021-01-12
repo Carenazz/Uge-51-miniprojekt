839ccba94165d6b95fba81bff30c89c89342f3ee
Carenazz <73272083+Carenazz@users.noreply.github.com>
2021-01-12 14:40:21 +0100

    Update BestillingsMenu.cs

--- Uge 51 - miniprojekt/BestillingsMenu.cs
+++ Uge 51 - miniprojekt/BestillingsMenu.cs
@@ -394,6 +394,13 @@ namespace Uge_51___miniprojekt
                 rabat = true;
                 iRabat += 20;
             }
+            else if (antalPizza % 2 == 0 && antalSoda % 2 == 0)
+            {
+                totalSum.Text = "Sum: " + sum.ToString() + "Kr.";
+                rabatTxt.Text = "Rabat: 20.-";
+                rabat = true;
+                iRabat += 20;
+            }
         }
         #endregion
     }
