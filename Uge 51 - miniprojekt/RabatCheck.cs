6a77963721bf305d1f51fbb400012467060459d2
Carenazz <73272083+Carenazz@users.noreply.github.com>
2021-01-14 09:29:31 +0100

    Testing - ny kode vindue.

---
+++ Uge 51 - miniprojekt/RabatCheck.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uge_51___miniprojekt
+{
+    public partial class RabatCheck : Form
+    {
+        // Testing - Rabat
+
+        public bool rabat = false;
+
+        public void Rabat()
+        {
+            if (antalPizza >= 2 && antalSoda >= 2 && rabat == false)
+            {
+                totalSum.Text = "Sum: " + sum.ToString() + "Kr.";
+                rabatTxt.Text = "Rabat: " + iRabat + " Kr.";
+                rabat = true;
+                iRabat += 20;
+            }
+        }
+    }
+}
